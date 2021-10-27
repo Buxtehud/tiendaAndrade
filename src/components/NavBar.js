d6a192ed49d3d5e9ff3e69e3c2eb66361b4fdc9f
Buxtehud <andres.anreyes@gmail.com>
2021-10-27 18:25:54 -0300

    Componente NavBar añadido

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,19 @@
-function NavBar(){
+import logo from "../assets/logo.jpg"
 
+function NavBar(){
+    return (
+    <nav className="navContainer">
+        <div>
+            <img src={logo}></img>
+        </div>
+        <div className="navLinkContainer">
+            <a href="#" className="navLink">Home</a>
+            <a href="#" className="navLink">Products</a>
+            <a href="#" className="navLink">About Us</a>
+            <a href="#" className="navLink">Contact</a>
+        </div>
+    </nav>
+    )
 }
 
 export default NavBar;
